5282378bdb34b1e9c1b8e400bd7deb2dcc3203a5
cheroliv <cheroliv92@gmail.com>
2019-06-25 03:09:15 +0200

    ___

--- src/main/groovy/com/cheroliv/fiber/config/Config.groovy
+++ src/main/groovy/com/cheroliv/fiber/config/Config.groovy
@@ -9,33 +9,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
-//import groovy.transform.CompileStatic
-//import org.springframework.boot.autoconfigure.domain.EntityScan
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.context.annotation.Import
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-//import org.springframework.jdbc.datasource.DriverManagerDataSource
-//import org.springframework.orm.jpa.JpaTransactionManager
-//import org.springframework.orm.jpa.JpaVendorAdapter
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
-//import org.springframework.orm.jpa.vendor.Database
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
-//import org.springframework.transaction.PlatformTransactionManager
-//import org.springframework.transaction.annotation.EnableTransactionManagement
-//import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
-//
-//import javax.sql.DataSource
-//import javax.validation.Validator
 @CompileStatic
 @Configuration
 @ComponentScan("com.cheroliv.fiber")
-//@EnableConfigurationProperties([FiberProperties.class])
-//@EnableTransactionManagement
-//@EnableJpaRepositories("com.cheroliv.fiber.dao")
-//@EntityScan("com.cheroliv.fiber.domain")
-//@Import([CacheConfiguration.class])
-//@EnableConfigurationProperties([FiberProperties.class])
 @EnableCaching
 @EnableFeignClients(basePackages = "com.cheroliv.fiber")
 @EnableSwagger2
